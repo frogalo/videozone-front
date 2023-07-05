7052d6bae34aa3ee64d6e129976758cd285d11b7
Jakub Urbanski <s23525@pjwstk.edu.pl>
2023-07-05 21:37:18 +0200

    working video uploading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleUploadClick = async () => {
+        // Open file explorer
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = 'video/*'; // Specify accepted file types as video files
+        input.onchange = (event) => {
+            const file = event.target.files[0];
+            setSelectedFile(file);
+        };
+        input.click();
+    };
+
+    const uploadVideo = async () => {
+        if (selectedFile) {
+            const formData = new FormData();
+            formData.append('file', selectedFile);
+
+            try {
+                await axios.post('http://localhost:8081/videos/upload', formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
+                });
+                console.log('Video uploaded successfully.');
+                // Do something after successful upload
+            } catch (error) {
+                console.error('Error uploading video:', error);
+                // Handle error
+            }
+        } else {
+            console.log('No file selected');
+        }
+    };
+
+    return (
+        <div>
+            <button onClick={handleUploadClick}>Upload</button>
+            {selectedFile && <p>Selected file: {selectedFile.name}</p>}
+            <button onClick={uploadVideo}>Submit</button>
+        </div>
+    );
 }
 
 export default App;
